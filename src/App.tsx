5479220560b37bdc9b32ded759edccae16ff1c4c
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-06 23:29:12 +0300

    create basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Flex } from "@chakra-ui/react";
+import { QueryClientProvider, QueryClient } from "react-query";
+import EpisodeContainer from "./components/EpisodeContainer";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const queryClient = new QueryClient();
+
 function App() {
-	return <div>test</div>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Flex direction="column" h="100vh" position="relative">
+				<Navbar />
+				<EpisodeContainer />
+				<Footer />
+			</Flex>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
