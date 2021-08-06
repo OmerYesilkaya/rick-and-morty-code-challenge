903a35a771e21828e20d900ac27e09a104bdacf8
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-06 22:39:45 +0300

    wrap App with ChakraProvider and create global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const styles = {
+	global: {
+		body: {
+			fontFamily: "Secular One, sans-serif;",
+			color: "#e9f7fa",
+			bg: "gray.800",
+		},
+	},
+};
+
+const config: ThemeConfig = {
+	initialColorMode: "light",
+	useSystemColorMode: false,
+};
+
+const theme = extendTheme({ styles, config });
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider theme={theme}>
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
