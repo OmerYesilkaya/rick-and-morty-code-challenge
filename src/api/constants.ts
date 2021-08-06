57fa1fa9eb4fce382b28012a87d9b3c9edcb6bcd
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-06 22:38:53 +0300

    create api constants

---
+++ src/api/constants.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://rickandmortyapi.com/api";
+
+export const API = {
+	EPISODES: BASE_URL + "/episodes",
+	LOCATIONS: BASE_URL + "/locations",
+	CHARACTERS: BASE_URL + "/characters",
+};
