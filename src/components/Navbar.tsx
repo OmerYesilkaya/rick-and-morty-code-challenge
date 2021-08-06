fa4ccd22b0f8444685f92ed4c81133992713ae01
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-06 22:40:04 +0300

    create basic navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Navbar() {
+	return (
+		<Box p="1em" shadow="lg" w="100%" bg="gray.700">
+			<Text fontSize="2xl">Rick and Morty Episodes</Text>
+		</Box>
+	);
+}
