a54662604afdb4fd356b11a1738a1974425bc41d
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-07 19:58:13 +0300

    add navigation to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,33 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import { QUERY_TYPE } from "api/constants";
+import { useHistory, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+	const history = useHistory();
+	const location = useLocation();
+
+	const currentPath = location.pathname.split("/")[1];
 	return (
-		<Box p="1em" shadow="lg" w="100%" bg="gray.700">
+		<Flex p="1em" shadow="lg" w="100%" bg="gray.700" justify="space-between" align="center">
 			<Text fontSize="2xl">Rick and Morty Code Challenge</Text>
-		</Box>
+
+			<Flex justify="space-between">
+				<Text
+					mr="1em"
+					cursor="pointer"
+					onClick={() => history.push("/character/1")}
+					textDecoration={currentPath === QUERY_TYPE.CHARACTERS ? "underline" : "none"}
+				>
+					Characters
+				</Text>
+				<Text
+					cursor="pointer"
+					onClick={() => history.push("/episode")}
+					textDecoration={currentPath === QUERY_TYPE.EPISODES ? "underline" : "none"}
+				>
+					Episodes
+				</Text>
+			</Flex>
+		</Flex>
 	);
 }
