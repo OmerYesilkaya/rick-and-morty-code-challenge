bddbea7f0b72c6aec5bfd49436510bf5ba759b98
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-07 14:57:38 +0300

    fix minor footer and navbar visual errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer() {
 	return (
-		<Box w="100%" bg="gray.600" p="1em" color="gray.300" position="absolute" bottom={0}>
+		<Box w="100%" bg="gray.600" p="1em" color="gray.300">
 			<Text textAlign="right">
 				github repo @{" "}
 				<a href="https://github.com/OmerYesilkaya/bitexen-code-challenge" target="_blank" rel="noreferrer">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 export default function Navbar() {
 	return (
 		<Box p="1em" shadow="lg" w="100%" bg="gray.700">
-			<Text fontSize="2xl">Rick and Morty Episodes</Text>
+			<Text fontSize="2xl">Rick and Morty Code Challenge</Text>
 		</Box>
 	);
 }
