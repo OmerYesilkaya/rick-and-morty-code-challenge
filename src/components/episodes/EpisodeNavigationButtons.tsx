cf40c3f09530b5506c9b20f59c03ff9086a18970
OmerYesilkaya <omer.yesilkaya@cmosrealitylab.com>
2021-08-07 14:56:05 +0300

    navigation buttons for episode navigation

---
+++ src/components/episodes/EpisodeNavigationButtons.tsx
@@ -0,0 +1,33 @@
+import { Button, Center } from "@chakra-ui/react";
+import { AllEpisodesResponse } from "models/Episode";
+
+type NavigationButtonProps = {
+	setPage: React.Dispatch<React.SetStateAction<number>>;
+	data: AllEpisodesResponse;
+};
+
+export default function EpisodeNavigationButtons({ setPage, data }: NavigationButtonProps) {
+	return (
+		<Center position="absolute" bottom="3em" w="100%">
+			<Button
+				w="10%"
+				onClick={() => setPage((prev) => prev - 1)}
+				colorScheme="pink"
+				variant={data.info.prev ? "solid" : "outline"}
+				isDisabled={!data.info.prev}
+			>
+				Prev
+			</Button>
+			<Button
+				ml="1em"
+				w="10%"
+				onClick={() => setPage((prev) => prev + 1)}
+				colorScheme="pink"
+				variant={data.info.next ? "solid" : "outline"}
+				isDisabled={!data.info.next}
+			>
+				Next
+			</Button>
+		</Center>
+	);
+}
